f699dee3e1f56ee7fbb127a0e6aa3188de7f421b
NinjaBitroom <gabrielbitercourt@gmail.com>
2023-10-23 20:36:12 -0400

    profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,62 @@
-
 import 'package:flutter/material.dart';
 import 'package:listas/entidades/pessoa.dart';
+import 'package:listas/pages/profile.dart';
 import 'package:listas/repository/pessoarepository.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
-
-  static final listaPessoas = PessoaRepository.listaPessoas;
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    verPerfil(Pessoa pessoa) {
-      Navigator.push(context, route);
-    }
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          appBar: AppBar(
-            leading: const Icon(
-              Icons.menu,
-              size: 30,
-            ),
-            title: const Center(
-              child: Text("Meu primeiro app"),
-            ),
-          ),
-          body: ListView.builder(
-              itemBuilder: (BuildContext context, int i) {
-                return Card(
-                  child: ListTile(
-                    tileColor: Colors.lightBlueAccent,
-                    leading: CircleAvatar(
-                      backgroundImage: AssetImage(listaPessoas[i].foto),
-                    ),
-                    title: Text(listaPessoas[i].nome),
-                    trailing: Text(listaPessoas[i].idade.toString()),
-                    onTap: verPerfil,
-                  ),
-                );
-              },
-              itemCount: listaPessoas.length)),
+      home: Main(),
     );
+  }
+}
+
+class Main extends StatelessWidget {
+  static final listaPessoas = PessoaRepository.listaPessoas;
+
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: const Icon(
+            Icons.menu,
+            size: 30,
+          ),
+          title: const Center(
+            child: Text("Meu primeiro app"),
+          ),
+        ),
+        body: ListView.builder(
+            itemBuilder: (BuildContext context, int i) {
+              return Card(
+                child: ListTile(
+                  tileColor: Colors.lightBlueAccent,
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(listaPessoas[i].foto),
+                  ),
+                  title: Text(listaPessoas[i].nome),
+                  trailing: Text(listaPessoas[i].idade.toString()),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) =>
+                              const ProfileInfo()),
+                    );
+                  },
+                ),
+              );
+            },
+            itemCount: listaPessoas.length));
   }
 }
