990c28997ef89a096faf9fb36dc8b8b9f7cf85e7
NinjaBitroom <gabrielbitercourt@gmail.com>
2023-11-07 14:21:46 -0400

    Atualize as imagens e remova páginas não usadas

--- lib/repository/pessoarepository.dart
+++ lib/repository/pessoarepository.dart
@@ -3,17 +3,17 @@ import 'package:listas/entidades/pessoa.dart';
 class PessoaRepository {
   static List<Pessoa> listaPessoas = [
     Pessoa(
-      foto: 'images/foto1.webp',
+      foto: 'images/foto1.jpg',
       nome: 'João',
       idade: 20,
     ),
     Pessoa(
-      foto: 'images/foto2.webp',
+      foto: 'images/foto2.jpg',
       nome: 'José',
       idade: 30,
     ),
     Pessoa(
-      foto: 'images/foto3.jfif',
+      foto: 'images/foto3.jpg',
       nome: 'Maria',
       idade: 22,
     ),
